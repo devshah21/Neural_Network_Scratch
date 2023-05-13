7f46e86be98f171e69c4a51c7676c53c47147ded
devshah21 <mail2devshah@gmail.com>
2023-05-12 21:43:16 -0400

    implemented one hot encoding

--- loss.py
+++ loss.py
@@ -22,3 +22,19 @@ loss = -(math.log(softmax_out[0])*one_hot_encoding[0] +
                 math.log(softmax_out[2])*one_hot_encoding[2])
 
 print(loss)
+
+soft_outputs = np.array([[0.7, 0.1, 0.2], 
+                    [0.1, 0.5, 0.4],
+                    [0.02, 0.9, 0.08]])
+
+class_targs = [0,1,1]
+
+#print(soft_outputs[[0, 1, 2], class_targs])
+
+print(soft_outputs[range(len(soft_outputs)), class_targs])
+
+new_loss = -np.log(soft_outputs[range(len(soft_outputs)), class_targs])
+
+avg_loss = np.mean(new_loss)
+
+print(new_loss)
