137965794205b9112839b06fb14ada88dbb78841
devshah21 <mail2devshah@gmail.com>
2023-06-09 10:03:08 -0400

    re-created dense layer w/ parent class Layer

---
+++ dense.py
@@ -0,0 +1,17 @@
+from layer import Layer
+import numpy as np
+
+
+class Dense(Layer):
+    
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
+        self.bias = np.random.randn(output_size, 1)
+        
+    def forward(self, input):
+        self.input = input
+        return np.dot(self.weights, self.input) + self.bias
+    
+    def backward(self, output_gradient, learning_rate):
+        pass
+    
---
+++ layer.py
@@ -0,0 +1,16 @@
+# parameters updated w/ partial derivatives and gradient descent
+# chain rule
+
+class Layer:
+    
+    def __init__(self):
+        self.input = None
+        self.output = None
+        
+    def forward(self, input):
+        pass
+    
+    def backward(self, output_gradient, learning_rate):
+        pass
+    
+        
